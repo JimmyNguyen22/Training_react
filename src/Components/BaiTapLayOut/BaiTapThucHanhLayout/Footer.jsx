af0c703e61ed5fd91d5a9fa745316dc67b02ff1d
jimmy <vphu456@gmail.com>
2022-08-14 19:42:26 +0700

    bai tap lauout

--- src/Components/BaiTapLayOut/BaiTapThucHanhLayout/Footer.jsx
+++ src/Components/BaiTapLayOut/BaiTapThucHanhLayout/Footer.jsx
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 
 export default class Footer extends Component {
   render() {
-      return <div>
-         
-    </div>;
+    return (
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright © Your Website 2022
+          </p>
+        </div>
+      </footer>
+    );
   }
 }
--- src/Components/BaiTapLayOut/BaiTapThucHanhLayout/Header.jsx
+++ src/Components/BaiTapLayOut/BaiTapThucHanhLayout/Header.jsx
@@ -3,69 +3,43 @@ import React, { Component } from "react";
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-          <a className="navbar-brand" href="#">
-            Navbar
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container px-lg-5">
+          <a className="navbar-brand" href="#!">
+            Start Bootstrap
           </a>
           <button
-            className="navbar-toggler d-lg-none"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
-            data-bs-target="#collapsibleNavId"
-            aria-controls="collapsibleNavId"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
             aria-expanded="false"
             aria-label="Toggle navigation"
-          />
-          <div className="collapse navbar-collapse" id="collapsibleNavId">
-            <ul className="navbar-nav me-auto mt-2 mt-lg-0">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">
-                  Home <span className="visually-hidden">(current)</span>
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#!">
+                  Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link" href="#!">
+                  About
                 </a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="dropdownId"
-                  data-bs-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  Dropdown
+              <li className="nav-item">
+                <a className="nav-link" href="#!">
+                  Contact
                 </a>
-                <div className="dropdown-menu" aria-labelledby="dropdownId">
-                  <a className="dropdown-item" href="#">
-                    Action 1
-                  </a>
-                  <a className="dropdown-item" href="#">
-                    Action 2
-                  </a>
-                </div>
               </li>
             </ul>
-            <form className="d-flex my-2 my-lg-0">
-              <input
-                className="form-control me-sm-2"
-                type="text"
-                placeholder="Search"
-              />
-              <button
-                className="btn btn-outline-success my-2 my-sm-0"
-                type="submit"
-              >
-                Search
-              </button>
-            </form>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
